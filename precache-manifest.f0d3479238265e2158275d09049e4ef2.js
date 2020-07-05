ac0a89e887e25c855410827abe16c74446e88f39
Lian Liu <lliu97d@gmail.com>
2020-07-05 00:46:35 -0700

    Updates

---
+++ precache-manifest.f0d3479238265e2158275d09049e4ef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794fcda38e3054b0217c824dce99d4ca",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "d69667ae6e64fee56216",
+    "url": "/react-tutorial/static/css/main.b044d04a.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69667ae6e64fee56216",
+    "url": "/react-tutorial/static/js/main.edf48b08.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
